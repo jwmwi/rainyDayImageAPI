d664f1f4b93ebd5020e151bb1c3644201abf7c02
Jeff Miller <jwmtri@gmail.com>
2022-12-28 09:55:38 -0600

    still playing with images in memory adding multiple results output page at this point moving stuff  around  for learning purposes.

--- api-backend/app.py
+++ api-backend/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, Response, request
 from werkzeug.utils import secure_filename
 
 # obviously don't do this, fun for testing
-allimages = []
+# allimages = []
 
 app = Flask(__name__)
+
+# until we figure out how we want to setup files.
+allimages = []
 
 @app.route('/')
 def hello():
@@ -19,13 +22,44 @@ def upload():
     filename = secure_filename(picture.filename)
     mimetype = picture.mimetype
     image = picture.read()
-    allimages.append([image,filename,mimetype])
-    return 'image uploaded', 200
+    j = '{ filename = "' + filename + '", mimetype = "' + mimetype + '" } '
+    allimages.append([image,filename,mimetype,j])
+    return 'image uploaded' + j , 200
 
 @app.route('/lastimage')
 def getlastimage():
-    last = allimages[-1]
-    return Response(last[0], last[2])
+    lastid=len(allimages)-1
+    return getImagebyID(lastid)
+
+@app.route('/image/<int:id>')
+def getImagebyID(id):
+    if id < len(allimages):
+        return Response(allimages[id][0], mimetype=allimages[id][2])
+    else:
+        return "no such image", 400
+
+@app.route('/imagelist')
+def showimages():
+    return render_template(
+        'results.html', results=listimages()
+        )
+
+def listimages():
+    results = []
+    for i in range(len(allimages)):
+        ## or prepend /image/
+        link = str(i)
+        results.append(link)
+    return results
+    ## figure out what to return
+    ## array of links for jinja2 
+
+@app.route('/jdump')
+def dumpJ():
+    s=""
+    for i in allimages:
+        s=s+i[3]+'\n'
+    return Response(s, 200)
 
 if __name__ == "__main__":
     app.run()
