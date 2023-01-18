ea5b8b678a1998dfa555f4f9b19be9601d0f27cd
Jeff Miller <jwmtri@gmail.com>
2023-01-17 23:13:42 -0600

    all over the place time to start adding ES connection add image saving.

--- api-backend/app.py
+++ api-backend/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, Response, request
 from werkzeug.utils import secure_filename
+import hashlib
+import os
 
 # obviously don't do this, fun for testing
 # allimages = []
 
 app = Flask(__name__)
+
+## look for env vars
+es_server = os.environ.get('CONFIG_ES_SERVER', "127.0.0.1")
+es_port = os.environ.get('CONFIG_ES_PORT', "9200")
+es_index = os.environ.get('CONFIG_ES_INDEX', "default_test_index")
+es_user = os.environ.get('CONFIG_ES_USER', "elastic")
+es_password = os.environ.get('CONFIG_ES_PW', "bad_password")
+
+base_image_dir = "/images"
 
 # until we figure out how we want to setup files.
 allimages = []
@@ -22,8 +33,10 @@ def upload():
     filename = secure_filename(picture.filename)
     mimetype = picture.mimetype
     image = picture.read()
-    j = '{ filename = "' + filename + '", mimetype = "' + mimetype + '" } '
-    allimages.append([image,filename,mimetype,j])
+    uniquehash = hashlib.md5(image)
+#    j = '{ filename = "' + filename + '", mimetype = "' + mimetype + '" } '
+    j = '{ filename = "' + filename + '", mimetype = "' + mimetype + '", hash = "' + uniquehash.hexdigest() + '" } '
+    allimages.append([image,filename,mimetype,j,uniquehash.hexdigest()])
     return 'image uploaded' + j , 200
 
 @app.route('/lastimage')
@@ -53,6 +66,23 @@ def listimages():
     return results
     ## figure out what to return
     ## array of links for jinja2 
+
+@app.route('/save')
+def saveimages():
+    for i in range(len(allimages)):
+        image,filename,mimetype,j,uniquehash = allimages[i]
+        print("filename :" + filename)
+        print("j = " + j )
+        print("mime = " + mimetype)
+        print("hash = " + uniquehash )
+        print()
+#        open(rw, base_image_dir + "/" + uniquehash )
+        with open(base_image_dir + "/" + uniquehash , 'wb') as f:
+            f.write(image)
+        with open(base_image_dir + "/" + uniquehash + ".json" , 'w') as f:
+            f.write(j)
+    return showimages()
+
 
 @app.route('/jdump')
 def dumpJ():
